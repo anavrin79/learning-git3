e51136e51ae5329f4dd527271959e98463098e49
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2021-12-07 10:57:16 +0100

    sort_list_of_tuples_lambda.py

--- sort_list_of_tuples_lambda.py
+++ sort_list_of_tuples_lambda.py
@@ -5,6 +5,9 @@ shopping_items = [
 ]
 
 def get_index_1_tuple_element(given_tuple):
+    """
+    Oto opis funkcji ... stworzona po to aby pokazac jak działa lambda, robi ona to samo co wyrażenie lambda poniżej, czyli tak na prawdę wskazuje po którym elemencie krotki należy posortować listę krotek ...
+    """
     return given_tuple[1]
 
 sorted_items = sorted(shopping_items, key=get_index_1_tuple_element)
@@ -13,3 +16,4 @@ print(sorted_items)
 #sortowanie - użyciem wyrażenia lambda
 sorted_items = sorted(shopping_items, key=lambda given_tuple: given_tuple[0])
 print(sorted_items)
+help(get_index_1_tuple_element)
