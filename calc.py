39894f7a7c86b53e351dd20eeadb7c6cf7795680
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2021-12-08 15:55:32 +0100

    calc.py

---
+++ calc.py
@@ -0,0 +1,29 @@
+import sys
+
+operation = input ("Podaj działanie [+,-,*,/]: ")
+
+first = input ("Podaj pierwszy składnik: ")
+second = input ("Podaj drugi składnik: ")
+
+firstnum = float(first)
+secondnum = float(second)
+
+if (operation=="+"):
+    print ("działanie: " + operation)
+    result = firstnum + secondnum
+    print ("Obliczam :"  + str(first) + str(operation) + str(second) + " = " + str(result))
+
+elif (operation=="-"):
+    print ("działanie: " + operation)
+    result = firstnum - secondnum
+    print ("Obliczam :"  + str(first) + str(operation) + str(second) + " = " + str(result))
+
+elif (operation=="*"):
+    print ("działanie: " + operation)
+    result = firstnum * secondnum
+    print ("Obliczam :"  + str(first) + str(operation) + str(second) + " = " + str(result))
+
+elif (operation=="/"):
+    print ("działanie: " + operation)
+    result = firstnum / secondnum
+    print ("Obliczam :"  + str(first) + str(operation) + str(second) + " = " + str(result))
