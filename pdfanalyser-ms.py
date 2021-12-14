c8051d92dab87655649985f9459b44a8b2143acf
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2021-12-14 10:42:10 +0100

    pdfanalyser-ms.py

---
+++ pdfanalyser-ms.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import pathlib
+import math
+from PyPDF2 import PdfFileReader
+
+def getSize(sizeMatrix):
+        """
+        Returns matrix with sizes from inches recalculated to mm.
+        It assumes that pdf.getPage(0).mediaBox returns always same matrix
+        """
+        widthInMm = sizeMatrix[(2)]*0.352
+        heightInMm = sizeMatrix[(3)]*0.352
+        mmSizes = []
+        #mmSizes = list(mmSizes)
+        mmSizes.append(widthInMm)
+        mmSizes.append(heightInMm)
+        return mmSizes
+
+def getNoOfA4Formats(mmSizes):
+        """
+        Returns no. of A4 formats of a given page 
+        """
+        A4 = math.ceil (mmSizes[0]*mmSizes[1]/62370)
+        return A4
+
+
+path = False
+
+if len(sys.argv) > 1:
+        path = sys.argv[1]
+
+if (path):
+        listOfFiles = os.listdir(path)
+else:        
+        listOfFiles = os.listdir(pathlib.Path().resolve())
+
+pathSlashed = path + "/"
+
+print (listOfFiles)
+a4formats = []
+
+for pdfFile in listOfFiles:
+        absPathFile = pathSlashed + pdfFile
+        
+        with open(absPathFile, 'rb') as f:
+                pdf = PdfFileReader(f)
+                pagesNo = pdf.getNumPages()
+                sizeMatrix = pdf.getPage(0).mediaBox
+                mmSizes = getSize(sizeMatrix)
+                #print(getNoOfA4Formats(mmSizes))
+                a4formats.append(getNoOfA4Formats(mmSizes))
+
+print(a4formats)
+
+#print (getSize(sizeMatrix))
+#print (getNoOfA4Formats(mmSizes))
