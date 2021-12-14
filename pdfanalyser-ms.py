e39e21e0b81aeda90ef4fc091a15eed1610723b3
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2021-12-14 14:37:04 +0100

    handling colors not working seems to be too complicated for me at the present ... removed parts of code with color handling, focus on getting rid off warrnings like PdfReadWarning: Xref table not zero-indexed. ID numbers for objects will be corrected. [pdf.py:1736]

--- pdfanalyser-ms.py
+++ pdfanalyser-ms.py
@@ -2,16 +2,19 @@ import sys
 import os
 import pathlib
 import math
-import minecart
 from PyPDF2 import PdfFileReader
+
 
 def getSize(sizeMatrix):
         """
         Returns matrix with sizes from inches recalculated to mm.
         It assumes that pdf.getPage(0).mediaBox returns always same matrix
         """
-        widthInMm = sizeMatrix[(2)]*0.352
-        heightInMm = sizeMatrix[(3)]*0.352
+        #print (sizeMatrix)
+        
+        widthInMm = float(sizeMatrix[(2)])*0.352
+        heightInMm = float(sizeMatrix[(3)])*0.352
+        #exit()
         mmSizes = []
         #mmSizes = list(mmSizes)
         mmSizes.append(widthInMm)
@@ -31,20 +34,6 @@ def totalNoOfFormatsInDir(a4formats):
                 sumOfA4 += i
         return sumOfA4 
 
-def getFileColors(file):
-        """
-        returns all colors used in a pdf file - not working ...
-        """
-        colors = set()
-        document = minecart.Document(file)
-        page = document.get_page(0)
-        for shape in page.shapes:
-                if shape.fill:
-                        colors.add(shape.fill.color.as_rgb())
-        return colors
-        #for color in colors: print color
-
-
 path = False
 
 if len(sys.argv) > 1:
@@ -59,14 +48,13 @@ pathSlashed = path + "/"
 
 print (listOfFiles)
 a4formats = []
+counterOfFiles = 0
 
 for pdfFile in listOfFiles:
         absPathFile = pathSlashed + pdfFile
         
         with open(absPathFile, 'rb') as f:
-                ###getting info about colors in a file
-                #colors = getFileColors(f)
-                ###
+                counterOfFiles += 1
                 pdf = PdfFileReader(f)
                 pagesNo = pdf.getNumPages()
                 sizeMatrix = pdf.getPage(0).mediaBox
@@ -74,7 +62,8 @@ for pdfFile in listOfFiles:
                 #print(getNoOfA4Formats(mmSizes))
                 a4formats.append(getNoOfA4Formats(mmSizes))
 
-print (totalNoOfFormatsInDir(a4formats)) 
+print ("wszystkie pliki", counterOfFiles)
+print ("formaty A4: ", totalNoOfFormatsInDir(a4formats)) 
 
 
 #print(a4formats)
