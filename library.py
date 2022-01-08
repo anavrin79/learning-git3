74aa3048b88b9101472633e547cf403abb5966fd
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2022-01-08 13:39:28 +0100

    listowanie filmow i seriali  z podzialem na typ

--- library.py
+++ library.py
@@ -1,9 +1,14 @@
-class LibraryItem:
-    def __init__(self, title, howmanytaken):
-        self.title = title
-        self.howmanytaken = howmanytaken
+from typing import Counter
 
-    
+
+class LibraryItem:
+    def __init__(self, title):
+        self.title = title
+        #self.howmanytaken = howmanytaken
+
+    #variables
+        self.howmanytaken = 0
+
     def play(self, step=1):
         self.howmanytaken += step
         
@@ -14,8 +19,14 @@ class Movie(LibraryItem):
         self.gendre = gendre
         super().__init__(*args, **kwargs)
     
-    def get_movies(self, title):
-        self.title = title        
+    def get_movies(items):
+        counter = 0
+        print ('Movies: ')
+        #self.items = items
+        for i in items:
+            if isinstance(items[counter], Movie):
+                print (items[counter].title)
+            counter += 1
     
     def printdata(self, title):
         self.title = title
@@ -32,8 +43,13 @@ class Series(LibraryItem):
         self.season = season
         super().__init__(*args, **kwargs)
     
-    def get_series(self, title):
-        self.title = title        
+    def get_series(items):
+        counter = 0 
+        print ('Series: ')
+        for i in items:
+            if isinstance(items[counter], Series):
+                print (items[counter].title)
+            counter += 1
 
     def printdata(self, episode, season):
         self.episode = episode
@@ -46,30 +62,34 @@ class Series(LibraryItem):
         self.season = season
         return(f'{self.title} S0{self.season} E0{self.episode}')   
 
-movies = list()
+items = list()
 
-movies.append (Movie(title='Don\'t look up', pubyear='2021', gendre='catastrophic', howmanytaken=1))
-movies.append (Movie(title='Frozen', pubyear='2012', gendre='comedy', howmanytaken=2))
-movies.append (Movie(title='Blow up', pubyear='2001', gendre='drama', howmanytaken=3))
-
-
-series1 = Series(title='Breaking Up',howmanytaken=1, episode=1,season=1)
-series2 = Series(title='Korean series',howmanytaken=2, episode=2,season=1)
-series3 = Series(title='Whatever',howmanytaken=3, episode=3,season=1)
+items.append (Movie(title='Don\'t look up', pubyear='2021', gendre='catastrophic'))
+items.append (Movie(title='Frozen', pubyear='2012', gendre='comedy'))
+items.append (Movie(title='Blow up', pubyear='2001', gendre='drama'))
 
 
-print (movies[0].title)
-Movie.play(movies[0])
-Movie.play(movies[0])
-Movie.play(movies[0])
-print (movies[0].howmanytaken)
+items.append (Series(title='Breaking Up', episode=1,season=1))
+items.append (Series(title='Korean series', episode=2,season=1))
+items.append (Series(title='Whatever', episode=3,season=1))
 
-print (series1.title)
-Movie.play(series1)
-Movie.play(series1)
-print (series1.howmanytaken)
 
-print (Movie.printdata(movies[0], title='Don\'t look up'))
-print (Movie.printdata(movies[1], title='Don\'t look up'))
-print (Movie.printdata(movies[2], title='Don\'t look up'))
-print (Series.printdata(series1, 1, 1))
+#print (movies[0].title)
+Movie.play(items[0])
+Movie.play(items[0])
+Movie.play(items[0])
+#print (items[0].howmanytaken)
+
+#print (series1.title)
+Movie.play(items[0])
+Movie.play(items[0])
+
+#print (Movie.printdata(items[0], title='Don\'t look up'))
+#print (Movie.printdata(items[1], title='Frozen'))
+#print (Movie.printdata(items[2], title='Blow up'))
+#print (Series.printdata(items[0], 1, 1))
+
+
+#Movie.get_movies(Movie.movies, movies)
+Movie.get_movies(items)
+Series.get_series(items)
