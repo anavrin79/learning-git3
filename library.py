ddd995999d1bf517f804ed785b42b9f0ab3513ad
Marcin Staszkiewicz <marcinstaszkiewicz@gmail.com>
2022-01-09 19:16:12 +0100

    do sprawdzenia i problem z dodatkowym punktem

--- library.py
+++ library.py
@@ -1,5 +1,8 @@
-from typing import Counter
-
+#spytac czemu nie dziala mi zeby wyswietlil tylko liste filmow albo tylko liste seriali ... (ostatni punkt zadania - dodatkowe)
+#from typing import Counter
+from pprint import pprint
+import random
+#pprint(the_list)
 
 class LibraryItem:
     def __init__(self, title):
@@ -11,7 +14,51 @@ class LibraryItem:
 
     def play(self, step=1):
         self.howmanytaken += step
-        
+
+    def search(titlesearch, items):
+        #print (titlesearch)
+        matches = [p for p in items if titlesearch in p.title]
+
+        for searchtitle in matches:
+            print(f'{searchtitle.title}')
+    
+    def tentimes(items):
+        for i in range(10):
+            LibraryItem.generate_views(items)
+        #LibraryItem.generate_views(items)
+        #return
+
+    def generate_views(items):
+        chosen = random.randint(0,len(items)-1)
+        chosenstep = random.randint(1,100)
+        print (f'losowo: {items[chosen].title} ' )
+        LibraryItem.play(items[chosen], step=chosenstep)
+        print (f'{items[chosen].title} {items[chosen].howmanytaken}')
+
+    def top_titles(items):
+
+        newlist = sorted(items, key=lambda x: x.howmanytaken, reverse=True)
+
+        #def switchfun(category):
+        #    return {
+        #        'Movie': print(f'{newlist[i].title} {newlist[i].pubyear} {newlist[i].gendre} {newlist[i].howmanytaken}'),
+        #        'Series': print(f'{newlist[i].title} {newlist[i].season} {newlist[i].episode} {newlist[i].howmanytaken}')
+        #    }[category]
+
+        for i in range(len(newlist)):
+            #print (items[i].title)
+
+            #switchfun(category)
+
+            if isinstance(newlist[i], Movie):
+                #switchfun(category)
+                print(f'{newlist[i].title} {newlist[i].pubyear} {newlist[i].gendre} {newlist[i].howmanytaken}')
+            
+            if isinstance(newlist[i], Series):
+                print(f'{newlist[i].title} {newlist[i].season} {newlist[i].episode} {newlist[i].howmanytaken}')
+
+
+
 
 class Movie(LibraryItem):
     def __init__(self, pubyear, gendre, *args, **kwargs):
@@ -22,7 +69,7 @@ class Movie(LibraryItem):
     def get_movies(items):
         counter = 0
         print ('Movies: ')
-        #self.items = items
+        
         for i in items:
             if isinstance(items[counter], Movie):
                 print (items[counter].title)
@@ -48,7 +95,7 @@ class Series(LibraryItem):
         print ('Series: ')
         for i in items:
             if isinstance(items[counter], Series):
-                print (items[counter].title)
+                print (items[counter].title, items[counter].season, items[counter].episode)
             counter += 1
 
     def printdata(self, episode, season):
@@ -73,6 +120,10 @@ items.append (Series(title='Breaking Up', episode=1,season=1))
 items.append (Series(title='Korean series', episode=2,season=1))
 items.append (Series(title='Whatever', episode=3,season=1))
 
+#def myFunc(e):
+#  return e['title']
+#
+#items.sort(key=myFunc)
 
 #print (movies[0].title)
 Movie.play(items[0])
@@ -93,3 +144,14 @@ Movie.play(items[0])
 #Movie.get_movies(Movie.movies, movies)
 Movie.get_movies(items)
 Series.get_series(items)
+
+#pprint (items)
+#print(list(items))
+
+LibraryItem.search('ore', items)
+
+LibraryItem.generate_views(items)
+
+LibraryItem.tentimes(items)
+
+LibraryItem.top_titles(items)
